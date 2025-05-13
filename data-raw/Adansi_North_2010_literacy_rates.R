1e0b1e8fa1c729314911db014c9cd18db45c11e7
Ama Owusu-Darko <owusudaa@emea.roche.com>
2025-05-13 08:44:57 +0000

    literacy_dataset: Adansi North 2010 census data

--- data-raw/Adansi_North_2010_literacy_rates.R
+++ data-raw/Adansi_North_2010_literacy_rates.R
@@ -7,15 +7,19 @@
 #'     keep_md: TRUE
 #' ---
 ## Using a function in this package `rGhanaCensus`
-Adansi_North_2010_literacy_rates <- create_district_literacy_df(
+Adansi_North_2010_literacy_rates <- new_literacy_data(
   name_of_region = "Ashanti",
   name_of_district = "Adansi North",
   census_year = 2010,
-  total_literate_population = 56068,
-  total_not_literate_population = 23306,
-  number_literate_males = 31502,
-  number_literate_females = 24566
+  total_literate_population = 57399,
+  total_not_literate_population = 15713,
+
+  number_not_literate_males = 4999,
+  number_literate_males = 30669,
+
+  number_literate_females = 26730,
+  number_not_literate_females = 10714
 )
 
 usethis::use_data(Adansi_North_2010_literacy_rates, overwrite = TRUE)
-devtools::session_info()
+sessioninfo::session_info()
